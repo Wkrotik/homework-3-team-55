f934ba85dfe6f3f90c9414bccd5ecaa990f48490
NOERROR4646 <ahmedzade2021@gmail.com>
2024-11-23 03:18:02 +0400

    Added the jaw
    Wasn't rlly difficult, kinda getting used to it! :D

--- scripts.js
+++ scripts.js
@@ -6,19 +6,27 @@
 */
 
 // Global variables for Artist's position and orientation
-var x, y;
-var angle;
+let x, y;
+let angle;
+let x_startingPoint;
+let x_endPoint;
+let y_startingPoint;
+let y_endPoint;
+let x_controlPoint1;
+let y_controlPoint1;
+let x_controlPoint2;
+let y_controlPoint2;
 
 function radian(degree) {
     return degree * Math.PI / 180;
 }
 
-function moveForward(distance, context) {
+/* function moveForward(distance, context) {
     let a = radian(angle);
     x = x + distance * Math.cos(a);
     y = y + distance * Math.sin(a);
     context.lineTo(x, y);    
-}
+} 
 
 function turnRight(degree) {
     angle = angle - degree;
@@ -46,7 +54,7 @@ function DrawSpiral(context) {
         context.stroke();
         turnRight(89);
     }
-}
+} */
 
 drawBrawlStarsLogo = (context) => {
     x = context.canvas.width / 2;
@@ -58,8 +66,7 @@ drawBrawlStarsLogo = (context) => {
     context.fillStyle = 'rgb(250, 188, 36)';
     context.beginPath();
     context.arc(x, y, 345, radian(196), radian(8));
-    context.stroke();
-    context.fill();
+    // context.fill();
     x_startingPoint = x + 345 * Math.cos(radian(196));
     y_startingPoint = y + 345 * Math.sin(radian(196));
     x_endPoint = x + 345 * Math.cos(radian(8));
@@ -69,8 +76,34 @@ drawBrawlStarsLogo = (context) => {
     x_controlPoint2 = x + 510 * Math.cos(radian(56));
     y_controlPoint2 = y + 510 * Math.sin(radian(56));
     context.moveTo(x_startingPoint, y_startingPoint);
-    context.beginPath;
     context.bezierCurveTo(x_controlPoint1, y_controlPoint1, x_controlPoint2, y_controlPoint2, x_endPoint, y_endPoint);
+    // context.fill();
+    x_startingPoint = x + 310 * Math.cos(radian(140));
+    y_startingPoint = y + 260 * Math.sin(radian(140));
+    context.moveTo(x_startingPoint, y_startingPoint);
+    x_endPoint = x_startingPoint + 40 * Math.cos(radian(102));
+    y_endPoint = y_startingPoint + 40 * Math.sin(radian(102));
+    context.lineTo(x_endPoint, y_endPoint);
+    x_startingPoint = x_endPoint + 36 * Math.cos(radian(12));
+    y_startingPoint = y_endPoint + 36 * Math.sin(radian(12));
+    context.arc(x_startingPoint, y_startingPoint, 36, radian(192), radian(102), true);
+    x_startingPoint = x_startingPoint + 36 * Math.cos(radian(102));
+    y_startingPoint = y_startingPoint + 36 * Math.sin(radian(102));
+    x_endPoint = x_startingPoint + 324 * Math.cos(radian(12));
+    y_endPoint = y_startingPoint + 324 * Math.sin(radian(12));
+    context.lineTo(x_endPoint, y_endPoint);
+    x_startingPoint = x_endPoint + 36 * Math.cos(radian(282));
+    y_startingPoint = y_endPoint + 36 * Math.sin(radian(282));
+    context.arc(x_startingPoint, y_startingPoint, 36, radian(102), radian(12), true);
+    x_startingPoint = x_startingPoint + 36 * Math.cos(radian(12));
+    y_startingPoint = y_startingPoint + 36 * Math.sin(radian(12));
+    context.moveTo(x_startingPoint, y_startingPoint);
+    x_endPoint = x_startingPoint + 40 * Math.cos(radian(282));
+    y_endPoint = y_startingPoint + 40 * Math.sin(radian(282));
+    context.lineTo(x_endPoint, y_endPoint);
+    x_endPoint = x + 310 * Math.cos(radian(140));
+    y_endPoint = y + 260 * Math.sin(radian(140));
+    context.lineTo(x_endPoint, y_endPoint);
     context.stroke();
     context.fill();
 }
